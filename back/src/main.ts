4ae907f6096a4cab7109c3a49a2a8f317a5e1ae0
Sterenn35 <slh35@hotmail.fr>
2024-01-13 14:10:33 +0100

    Create microservices

--- back/src/main.ts
+++ back/src/main.ts
@@ -6,7 +6,7 @@ import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(
-      { 'origin':'http://localhost:4200' }
+      { 'origin':'http://localhost:80' }
     );
     
   const config = new DocumentBuilder()
