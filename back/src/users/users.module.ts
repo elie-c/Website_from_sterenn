1d2d44a6bf23aa8c3066ae9d5572c00812457e84
Sterenn35 <slh35@hotmail.fr>
2024-01-18 15:09:44 +0100

    Clean branch

--- back/src/users/users.module.ts
+++ back/src/users/users.module.ts
@@ -4,9 +4,25 @@ import { UsersService } from "./users.service";
 import { User } from "./user.entity";
 import { TypeOrmModule } from "@nestjs/typeorm/dist/typeorm.module";
 import { AssociationsModule } from "src/associations/associations.module";
+import {ClientsModule, Transport} from "@nestjs/microservices";
 
 @Module({
-    imports:[TypeOrmModule.forFeature([User])],
+    imports:[TypeOrmModule.forFeature([User]),
+        ClientsModule.register([
+            {
+                name: 'MAIL_SERVICE',
+                transport: Transport.RMQ,
+                options: {
+                    urls: [
+                        `amqp://${process.env.RABBIT_USER}:${process.env.RABBIT_PWD}@${process.env.RABBIT_HOST}:${process.env.RABBIT_PORT}`,
+                    ],
+                    queue: process.env.RABBIT_QUEUE,
+                    queueOptions: {
+                        durable: true,
+                    },
+                },
+            },
+        ]),],
     controllers: [UsersController],
     providers: [UsersService],
     exports:[UsersService]
